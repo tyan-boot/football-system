11e38e94ee245614b118dc470f9d9fa4ab0a6b88
tyan boot <tyanboot@outlook.com>
2018-05-08 14:38:01 +0800

    fix: swap param for RaceRepo

--- src/main/java/cn/edu/ncu/football/repo/RaceRepo.java
+++ src/main/java/cn/edu/ncu/football/repo/RaceRepo.java
@@ -5,5 +5,5 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface RaceRepo extends CrudRepository<Integer, Race> {
+public interface RaceRepo extends CrudRepository<Race, Integer> {
 }
