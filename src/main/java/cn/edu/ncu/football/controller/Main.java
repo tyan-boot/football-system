1e06113a1748376edaa718992c2ea4a66435fda8
tyan boot <tyanboot@outlook.com>
2018-05-17 22:14:07 +0800

    按日期筛选.

--- src/main/java/cn/edu/ncu/football/controller/Main.java
+++ src/main/java/cn/edu/ncu/football/controller/Main.java
@@ -14,6 +14,10 @@ import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.support.rowset.SqlRowSet;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
+import java.util.Calendar;
+import java.util.Date;
+
 @Service
 public class Main {
     @FXML
@@ -71,10 +75,12 @@ public class Main {
 
         subGroupType.setPromptText("男子甲组");
         subGroupType.getItems().addAll("男子甲组", "男子乙组", "女子组");
-        calenderTeam.setPromptText("4.1");
-        calenderTeam.getItems().addAll("4.1", "4.2", "4.3", "4.4", "4.5", "4.6", "4.7", "4.8", "4.9", "4.10", "4.11", "4.12", "4.13", "4.14", "4.15", "4.16", "4.17", "4.18", "4.19", "4.20", "4.21", "4.22", "4.23", "4.24", "4.25", "4.26");
-        calenderPlay.setPromptText("4.1");
-        calenderPlay.getItems().addAll("4.1", "4.2", "4.3", "4.4", "4.5", "4.6", "4.7", "4.8", "4.9", "4.10", "4.11", "4.12", "4.13", "4.14", "4.15", "4.16", "4.17", "4.18", "4.19", "4.20", "4.21", "4.22", "4.23", "4.24", "4.25", "4.26");
+
+        calenderTeam.getItems().addAll("5.1", "5.2", "5.3", "5.4", "5.5", "5.6", "5.7", "5.8", "5.9", "5.10", "5.11", "5.12", "5.13", "5.14", "5.15", "5.16", "5.17", "5.18", "5.19", "5.20", "5.21", "5.22", "5.23", "5.24", "5.25", "5.26");
+        calenderPlay.getItems().addAll("5.1", "5.2", "5.3", "5.4", "5.5", "5.6", "5.7", "5.8", "5.9", "5.10", "5.11", "5.12", "5.13", "5.14", "5.15", "5.16", "5.17", "5.18", "5.19", "5.20", "5.21", "5.22", "5.23", "5.24", "5.25", "5.26");
+
+        calenderTeam.getSelectionModel().select(0);
+        calenderPlay.getSelectionModel().select(0);
     }
 
     @FXML
@@ -95,15 +101,27 @@ public class Main {
 
     @FXML
     public void loadShotInfo(MouseEvent event) {
+        String dateInfo = calenderPlay.getSelectionModel().getSelectedItem();
+        String[] split = dateInfo.split("\\.");
+
+        Integer month = Integer.parseInt(split[0]);
+        Integer day = Integer.parseInt(split[1]);
+
+        LocalDate date = LocalDate.of(2018, month, day);
+
         SqlRowSet sqlRowSet = jdbcTemplate.queryForRowSet("select\n" +
                 "  player.name as player_name,\n" +
                 "  team.name   as team_name,\n" +
                 "  count(1)    as shot_count\n" +
-                "from player\n" +
-                "  left join shot_result on player_id = player.id\n" +
+                "from race\n" +
+                "  left join race_results on race.race_results_id = race_results.id\n" +
+                "  left join shot_result on shot_result.race_result_id = race_results.id\n" +
+                "  left join player on player.id = shot_result.player_id\n" +
                 "  left join team on player.team_id = team.id\n" +
-                "group by (player_id)\n" +
-                "order by shot_count desc");
+                "where hold_date = ? \n" +
+                "group by player.id\n" +
+                "order by shot_count desc;", date.toString());
+
 
         ObservableList<ShotRank> observableList = FXCollections.observableArrayList();
         Integer rank = 1;
